38cb9de11a66521a9e479c61847b1f09a15c2d9a
Porter Smith <flowbish@gmail.com>
2016-02-15 13:54:31 -0600

    begin parallel computations with threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 extern crate sdl2_image;
 extern crate image;
+extern crate scoped_threadpool;
 
 mod raytrace;
 use std::env;
@@ -14,8 +15,8 @@ use sdl2_image::{ImageRWops, INIT_PNG, INIT_JPG};
 use image::{Pixel, ImageBuffer, Rgba, RgbaImage, RgbImage, imageops};
 use std::path::Path;
 use std::sync::{Arc, Mutex};
-use std::thread;
-use std::sync::mpsc;
+use scoped_threadpool::Pool;
+use std::sync::mpsc::channel;
 
 /// Load in a buffer of pixel data from a png file and run a Gaussian filter
 fn blur_data(image: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> Vec<u8> {
@@ -77,40 +78,66 @@ fn main() {
         Err(err) => panic!("Failed to create renderer: {}", err),
     };
 
-    // Create surface to be drawn on by the raytracer
-    let mut my_surface = Surface::new(width, height, PixelFormatEnum::ARGB8888).unwrap();
 
-    // Create a raytracing state and run it a couple times
-    let mut state = raytrace::RaytraceState::new((width, height), &image_data[..], &blur_data[..], (350.0, 350.0));
-
-    let mut events = ctx.event_pump().unwrap();
 
     // Current frame rendered
     let mut iter = 1;
 
+    // Initialize pool of threads to render
+    let num_threads = 1;
+    let mut pool = Pool::new(num_threads);
+    let (tx, rx) = channel();
+
     // loop until we receive a QuitEvent or escape key pressed
-    'event: loop {
-        // poll_event returns the most recent event or NoEvent if nothing has happened
-        for event in events.poll_iter() {
-            match event {
-                Event::Quit{..} |
-                Event::KeyDown{keycode: Option::Some(Keycode::Escape), ..} =>
-                    break 'event,
-                _ => continue
+    let mut events = ctx.event_pump().unwrap();
+
+    pool.scoped(|scope| {
+        'event: loop {
+            // poll_event returns the most recent event or NoEvent if nothing has happened
+            for event in events.poll_iter() {
+                match event {
+                    Event::Quit{..} |
+                    Event::KeyDown{keycode: Option::Some(Keycode::Escape), ..} =>
+                        break 'event,
+                    _ => continue
+                }
             }
+
+            // // Create surface to be drawn on by the raytracer
+            // let surfaces: Vec<Surface> = (0..num_threads).map(|x| Surface::new(width, height, PixelFormatEnum::ARGB8888).unwrap()).collect();
+
+            // update buffers
+            for i in 0..num_threads {
+                let tx = tx.clone();
+                let idc = image_data.clone();
+                let bdc = image_data.clone();
+                scope.execute(move|| {
+                    // Create a raytracing state and run it a couple times
+                    let image_data = idc;
+                    let blur_data = bdc;
+                    let mut state = raytrace::RaytraceState::new((width, height), &image_data[..], &blur_data[..], (350.0, 350.0));
+                    let mut surface = Surface::new(width, height, PixelFormatEnum::ARGB8888).unwrap();
+                    surface.with_lock_mut(|data: &mut [u8]| {
+                        state.raytrace(data, iter);
+                        tx.send(data[..]).unwrap();
+                    });
+                });
+            };
+            iter += 1;
+
+            let my_surface = Surface::new(width, height, PixelFormatEnum::ARGB8888).unwrap();
+
+            // Copy texture onto renderer buffer
+            let mut rx_iter = rx.iter();
+            for i in 0..num_threads {
+                let a = rx_iter.next();
+            }
+
+            let my_texture = renderer.create_texture_from_surface(&my_surface).unwrap();
+            renderer.copy(&my_texture, None, None);
+
+            // Swap our buffer for the present buffer, displaying it.
+            renderer.present();
         }
-
-        // update buffer
-        my_surface.with_lock_mut(|data: &mut [u8]| {
-            state.raytrace(data, iter);
-        });
-        iter += 1;
-
-        // Copy texture onto renderer buffer
-        let my_texture = renderer.create_texture_from_surface(&my_surface).unwrap();
-        renderer.copy(&my_texture, None, None);
-
-        // Swap our buffer for the present buffer, displaying it.
-        renderer.present();
-    }
+    });
 }
